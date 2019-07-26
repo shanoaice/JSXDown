cb9b885d85f6af42b367610fac1159172348f117
KsRyY <andy200511@126.com>
2019-07-27 00:38:12 +0800

    Rebuils

--- packages/jsxdown/src/jsxdown.js
+++ packages/jsxdown/src/jsxdown.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import snarkdown from 'snarkdown'
-import { transform } from 'h2x-core'
-import jsx from 'h2x-plugin-jsx'
-import JsxParser from 'react-jsx-parser'
-export const JSXDown = function({ mdText, bindings }) {
-    if(!bindings) {
-        bindings = {};
-    }
+export const JSXDown = function({ mdText }) {
     const transformedHTML = snarkdown(mdText);
-    const transformedJSXStr = transform( transformedHTML, {plugins:[jsx]});
-    return (<JsxParser
-        bindings={bindings}
-        jsx={transformedJSXStr} />)
+    return (<div dangerouslySetInnerHTML={{__html: transformedHTML}} />)
 }
